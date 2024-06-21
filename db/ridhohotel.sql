753ea5d28badf4e260b0d12b502b1c987788a2d3
pngestu00 <pngestu0@gmail.com>
2024-06-21 23:20:58 +0700

    first init github

---
+++ db/ridhohotel.sql
@@ -0,0 +1,202 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 21 Jun 2024 pada 17.43
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ridhohotel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `emp_login`
+--
+
+CREATE TABLE `emp_login` (
+  `empid` int(100) NOT NULL,
+  `Emp_Email` varchar(50) NOT NULL,
+  `Emp_Password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `emp_login`
+--
+
+INSERT INTO `emp_login` (`empid`, `Emp_Email`, `Emp_Password`) VALUES
+(1, 'Admin@gmail.com', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `payment`
+--
+
+CREATE TABLE `payment` (
+  `id` int(30) NOT NULL,
+  `Name` varchar(30) NOT NULL,
+  `Email` varchar(30) NOT NULL,
+  `RoomType` varchar(30) NOT NULL,
+  `Bed` varchar(30) NOT NULL,
+  `NoofRoom` int(30) NOT NULL,
+  `cin` date NOT NULL,
+  `cout` date NOT NULL,
+  `noofdays` int(30) NOT NULL,
+  `roomtotal` double(8,2) NOT NULL,
+  `bedtotal` double(8,2) NOT NULL,
+  `meal` varchar(30) NOT NULL,
+  `mealtotal` double(8,2) NOT NULL,
+  `finaltotal` double(8,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `room`
+--
+
+CREATE TABLE `room` (
+  `id` int(30) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `bedding` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `roombook`
+--
+
+CREATE TABLE `roombook` (
+  `id` int(10) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Country` varchar(30) NOT NULL,
+  `Phone` varchar(30) NOT NULL,
+  `RoomType` varchar(30) NOT NULL,
+  `Bed` varchar(30) NOT NULL,
+  `Meal` varchar(30) NOT NULL,
+  `NoofRoom` varchar(30) NOT NULL,
+  `cin` date NOT NULL,
+  `cout` date NOT NULL,
+  `nodays` int(50) NOT NULL,
+  `stat` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `signup`
+--
+
+CREATE TABLE `signup` (
+  `UserID` int(100) NOT NULL,
+  `Username` varchar(50) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `staff`
+--
+
+CREATE TABLE `staff` (
+  `id` int(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `work` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `emp_login`
+--
+ALTER TABLE `emp_login`
+  ADD PRIMARY KEY (`empid`);
+
+--
+-- Indeks untuk tabel `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `roombook`
+--
+ALTER TABLE `roombook`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `signup`
+--
+ALTER TABLE `signup`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- Indeks untuk tabel `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `emp_login`
+--
+ALTER TABLE `emp_login`
+  MODIFY `empid` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `room`
+--
+ALTER TABLE `room`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT untuk tabel `roombook`
+--
+ALTER TABLE `roombook`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+
+--
+-- AUTO_INCREMENT untuk tabel `signup`
+--
+ALTER TABLE `signup`
+  MODIFY `UserID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
